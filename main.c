14b3453c8c1cd897a63330a8b643fb706239e7f6
su <wkdlfflxh@naver.com>
2021-08-23 23:51:06 +0900

    t1, fix main

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	double a = 1.1;
+	double a = 4.2;
 	double b = 4.2;
 
 	plus(a, b);
@@ -12,6 +12,6 @@ int main(void)
 	// div(a, b);
 
 	printf("%f + %f = %f\n", a, b, plus(a, b));
-	printf("%f - %f = %f\n", a, b, minus(a, b));
+	printf("%f - %f = %f\n", a, b, plus(a, b));
 	printf("%f * %f = %f\n", a, b, plus(a, b));
 }
