366b0b6c8463a53a21b318bf7fdfa78701cd2653
Euimin Chung <jane3079@korea.ac.kr>
2021-11-03 10:51:24 +0900

    conflict?

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 
 void functionA()
 {	
-	printf("Function A in main.\n");
+	printf("Function A in main2.\n");
 }
 
 void functionB()
 {
-	printf("Function B in main.\n");
+	printf("Function B in main2.\n");
 }
 
 /**
